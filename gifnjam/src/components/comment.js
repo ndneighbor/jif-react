cf8399373f3e4599a8b20faaa93cb9c3546b248f
Ashley Narcisse-Austin <Ashley_NarcisseAustin@ultimatesoftware.com>
2017-04-02 11:49:33 -0400

    finished comments

--- gifnjam/src/components/comment.js
+++ gifnjam/src/components/comment.js
@@ -5,7 +5,9 @@ export default class Comment extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			author: {}
+			author: {
+				username: 'Anonymous'
+			}
 		}
 	}
 
@@ -14,8 +16,23 @@ export default class Comment extends Component {
 			.then(res => this.setState({author: res.data}))
 	}
 	render() {
+		const {author} = this.state;
 		return (
-			<div />
+			<article className="media">
+				<figure class="media-left">
+			    <p class="image is-32x32">
+			      <img src={author.profile_photo}  style={{width: 54}}/>
+			    </p>
+			    @{author.username}
+			  </figure>
+				<div className="media-content" style={{paddingLeft: 15}}>
+					<div className="content">
+						<a  className="image">
+							<img src={this.props.gif} />
+						</a>
+					</div>
+				</div>
+			</article>
 		);
 	}
 }
